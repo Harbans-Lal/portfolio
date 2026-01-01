ecabb9b2277fc732b20619254e73ca9638ceba30
harbans <harbansl506@gmail.com>
2026-01-01 21:42:08 +0530

    profile update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -53,7 +53,7 @@ const About = () => {
                     </span>*/}
                     <img
                       className="w-32 h-32 md:w-40 md:h-40 rounded-full object-cover mx-auto"
-                      src="/profile.jpeg"
+                      src="/harbans-photo.jpeg"
                       alt="Profile Picture"
                     />
                   </div>
